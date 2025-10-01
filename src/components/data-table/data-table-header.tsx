fc9a08b85ce3129098622715119c771de3c6f67a
Maxime Gardier <m.gardierpro@gmail.com>
2025-10-01 10:11:43 +0200

    wip filter modal

--- src/components/data-table/data-table-header.tsx
+++ src/components/data-table/data-table-header.tsx
@@ -12,6 +12,19 @@ import type { Job } from "@/types/entity";
 import { useNavigate } from "react-router-dom";
 import type { FindAllJobParams } from "@/features/job/types/data-table";
 import type { Dispatch, SetStateAction } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { FilterTextField } from "./filter-text-field";
 
 interface DataTableHeaderProps {
   columns: Column<Job, unknown>[];
@@ -44,14 +57,91 @@ export const DataTableHeader = ({
       },
     });
 
+  const 
+
   return (
     <div className="md: flex  justify-between items-center py-4 mx-2">
       <div className="flex justify-center items-center gap-2">
-        <Button className="flex  gap-2  text-white" variant="blue">
-          <SlidersHorizontal />
-          FIlter
-        </Button>
-        <Button className="flex  gap-2  text-white" variant="blue" onClick={resetParams}>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="flex  gap-2  text-white" variant="blue">
+              <SlidersHorizontal />
+              FIlter
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md flex flex-col ">
+            <DialogHeader>
+              <DialogTitle>Filtres </DialogTitle>
+              <DialogDescription>Filtrer les candidatures.</DialogDescription>
+            </DialogHeader>
+            <div className="flex  flex-col justify-center items-center gap-4">
+
+              <FilterTextField 
+              {...{label,
+                defaultValue : params.jobTitle
+                
+              }}/>
+
+              <div className="grid flex-1 gap-2">
+                <Label htmlFor="link" className="">
+                  Recherche par titre
+                </Label>
+                <Input
+                type="text"
+                className="h-8 w-xs max-w-52 mx-auto mb-4"
+                value={params.jobTitle}
+                onChange={(e)=> setParams((prevParams)=> {return {
+                  ...prevParams,
+                  jobTitle : e.target.value
+                }}) }
+              />
+
+              </div>
+
+                            <div className="grid flex-1 gap-2">
+                <Label htmlFor="link" className="">
+                  Recherche par Entreprise
+                </Label>
+                <Input
+                type="text"
+                className="h-8 w-xs max-w-52 mx-auto mb-4"
+                value={params.jobTitle}
+               // onChange={handleChange}
+              />
+
+              </div>
+
+                                          <div className="grid flex-1 gap-2">
+                <Label htmlFor="link" className="">
+                  Egal au Statut
+                </Label>
+                <Input />
+
+              </div>
+
+                                                        <div className="grid flex-1 gap-2">
+                <Label htmlFor="link" className="">
+                  Par Postuler via 
+                </Label>
+                <Input />
+
+              </div>
+            </div>
+            <DialogFooter className="sm:justify-end">
+              <DialogClose asChild>
+                <Button className="flex  gap-2  text-white" variant="blue">
+                  Appliquer
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
+        <Button
+          className="flex  gap-2  text-white"
+          variant="blue"
+          onClick={resetParams}
+        >
           <RotateCcw />
           Reinitialiser
         </Button>
---
+++ src/components/data-table/filter-text-field.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { debounce } from "@/utils/utils";
+
+interface FilterTextFieldProps {
+  label: string;
+  defaultValue: string;
+  setFilter: (value: string) => void;
+}
+
+export const FilterTextField = ({
+  label,
+  defaultValue,
+  setFilter,
+}: FilterTextFieldProps) => {
+  const [value, setValue] = useState(defaultValue);
+  const debouncedSetFilter = debounce((value: string) => setFilter(value), 500);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValue(value);
+    debouncedSetFilter(value);
+  };
+  <div className="grid flex-1 gap-2">
+    <Label htmlFor="link" className="">
+      Recherche par titre
+    </Label>
+    <Input
+      type="text"
+      className="h-8 w-xs max-w-52 mx-auto mb-4"
+      value={value}
+      onChange={handleOnChange}
+    />
+  </div>;
+};
